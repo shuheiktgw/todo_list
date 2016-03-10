1f6323e5a6cde6d05818ccdfff0fe944945a6dc8
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-03-10 13:36:03 +0900

    changed sql into arel

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,10 +3,13 @@ class Task < ActiveRecord::Base
 
 	validates :name, presence: true
 
-	scope :should_work_on, ->{where("status <> ?", ":done")}
-	scope :rescently_done, -> {where(["status = ? and updated_at > ?", 2, 3.days.ago])}
+
 
 	enum urgency: {urgency_low: 0, urgency_middle: 1, urgency_high: 2}
 	enum importance: {importance_low: 0, importance_middle: 1, importance_high: 2}
 	enum status: {not_yet: 0, wip: 1, done: 2}
+
+	task_table = Task.arel_table
+	scope :should_work_on, ->{where(task_table[:status].not_eq(2))}
+	scope :rescently_done, ->{where(task_table[:status].eq(2), task_table[:updated_at].gt(3.days.ago))}
 end
