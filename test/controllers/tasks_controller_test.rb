126d651843a444d57c07d1212ba52d80d3f1eaa0
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-03-12 20:25:32 +0900

    add integration test for done regsitration

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@task = tasks(:test_task)
+	end
+
+
+	test "check done registration" do
+		get tasks_path
+		assert_difference "Task.done.count", 1 do
+			patch done_registration_tasks, checked_id: ["1"]
+		end
+		assert _redirected_to tasks_url
+	end
+
+
+
 end
