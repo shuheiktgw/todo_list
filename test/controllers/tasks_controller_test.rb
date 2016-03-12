961dedfadde4706439fbf33c7a6188ccc00fba94
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-03-12 20:29:22 +0900

    modified

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -2,19 +2,6 @@ require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
 
-	def setup
-		@task = tasks(:test_task)
-	end
-
-
-	test "check done registration" do
-		get tasks_path
-		assert_difference "Task.done.count", 1 do
-			patch done_registration_tasks, checked_id: ["1"]
-		end
-		assert _redirected_to tasks_url
-	end
-
 
 
 end
