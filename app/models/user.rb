aafcde516bb1551c3f275396c40b71754380db0e
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-03-14 12:33:24 +0900

    add rspec and modified class medhod

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
