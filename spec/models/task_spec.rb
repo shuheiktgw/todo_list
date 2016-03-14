4cce378df77eca9e020ec8a9c3e48c4e8cb9cb76
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-03-14 19:38:39 +0900

    add multiple data tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,16 +4,25 @@ describe Task do
 	describe "#done_regsitration" do
 		before do
 			@user = User.create(email: "test@test.com", password: "password")
-			@task = Task.create(user_id: @user.id, name: "test", urgency: 0, importance: 0, status: 0)
+			@task1 = Task.create(user_id: @user.id, name: "test", urgency: 0, importance: 0, status: 0)
+			@task2 = Task.create(user_id: @user.id, name: "test", urgency: 0, importance: 0, status: 0)
+			@task3 = Task.create(user_id: @user.id, name: "test", urgency: 0, importance: 0, status: 0)
 		end
+
 		it "Done registration" do
-			@task.done!
-			expect(@task.status).to eq "done"
+			@task1.done!
+			expect(@task1.done?).to be true
 		end
 
 		it "#done_multiple" do
-			Task.done_multiple(@user, [@task.id])
-			expect(@task.status).to eq "done"
+			Task.done_multiple(@user, [@task1.id, @task2.id, @task3.id])
+			expect(@task1.done? && @task2.done? && @task3.done?).to be true
+		end
+
+		it "#done_multiple(2nd data is invalid)" do
+			@task2.name=""
+			Task.done_multiple(@user, [@task1.id, @task2.id, @task3.id])
+			expect(@task1.done? || @task2.done? || @task3.done?).to be false
 		end
 	end
 end
