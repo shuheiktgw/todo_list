2c41299854a20b60cbaf49f007112a7c2af25a57
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-03-15 13:49:17 +0900

    Add factorygirl

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,10 +2,8 @@ require 'rails_helper'
 
 describe Task do
 	before do
-			@user = User.create(email: "test@test.com", password: "password")
-			@task1 = Task.create(user_id: @user.id, name: "test", urgency: 0, importance: 0, status: 0)
-			@task2 = Task.create(user_id: @user.id, name: "test", urgency: 0, importance: 0, status: 0)
-			@task3 = Task.create(user_id: @user.id, name: "test", urgency: 0, importance: 0, status: 0)
+			@user  = create(:user)
+			@task1, @task2, @task3 = create(:task), create(:task), create(:task)
 	end
 
 	describe "#done_regsitration" do
@@ -16,18 +14,17 @@ describe Task do
 	end
 
 	describe "#done_multiple"
-		it "3つのタスクを完了登録すること" do
+		xit "成功すればすべて完了になる" do
 			Task.done_multiple(@user, [@task1.id, @task2.id, @task3.id])
 			expect(@task1.reload.done?).to be true
 			expect(@task2.reload.done?).to be true
 			expect(@task3.reload.done?).to be true
 		end
 
-		it "3つのタスクのうち,2番めに無効な値を入れてトランザクションさせること" do
-			@task2.id=0
-			expect{Task.done_multiple(@user, [@task1.id, @task2.id, @task3.id])}.to raise_error
-			expect(@task1.reload.done?).to be false
-			expect(@task3.reload.done?).to be false
+		it "どれかが失敗するとすべてのステータスが変更されない" do
+			expect{Task.done_multiple(@user, [@task1.id, @task2.id = 0, @task3.id])}.to raise_error
+			expect(@task1.reload.not_yet?).to be true
+			expect(@task3.reload.not_yet?).to be true
 		end
 	end
 
